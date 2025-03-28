2382d7b24e70159b909ca5413be9e831b0ae91af
shreddedcandy <104097164+shreddedcandy@users.noreply.github.com>
2025-03-29 04:37:44 +0800

    Delete taskManagmentApp-main 2 directory

--- taskManagmentApp-main 2/__mocks__/react-native-vector-icons.js
+++
@@ -1,9 +0,0 @@
-const React = require("react");
-const { Text } = require("react-native");
-
-const MockIcon = (props) => {
-  return React.createElement(Text, props, props.name || "Icon");
-};
-
-module.exports = MockIcon;
-module.exports.default = MockIcon;
